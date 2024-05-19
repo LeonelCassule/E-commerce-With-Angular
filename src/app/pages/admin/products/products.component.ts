489a862fc190f764e66257bf193c76f96ffa44f9
LeonelCassule <leonelcassule1994@gmail.com>
2024-05-19 23:45:05 +0100

    feature of the Products component

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  isSidePanelVisible: boolean = false;
 
+  productObj: any = 
+    {
+      "productId": 0,
+      "productSku": " ",
+      "ProductName": "",
+      "productPrice": 0,
+      "productShortName": "",
+      "productDescription": "",
+      "createdDate": new Date(),
+      "deliveryTimeSpan": "",
+      "categoryId":0,
+      "productImageUrl": ""
+    }
+  
+
+    openSidePanel(){
+      this.isSidePanelVisible = true;
+    }
+
+    closeSidePanel(){
+    this.isSidePanelVisible = false;
+    }
 }
